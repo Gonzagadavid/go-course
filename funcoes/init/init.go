593382218ae46c998987b5c510b8c544bedd7e0a
gonzagadavid <david.blesses@hotmail.com>
2023-12-09 13:14:44 -0300

    feat: add func init

---
+++ funcoes/init/init.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func init() {
+	fmt.Println("inicializando...")
+}
+
+func main() {
+	fmt.Println("main...")
+}
+
+/*
+$ funcoes/init/init.go
+inicializando 2...
+inicializando...
+main...
+*/
